c481b070cfbd760550e1f474f922b03c6d0f20d2
Carlos Viana <carlosaraujoviana@outlook.com>
2021-12-29 22:58:11 -0500

    Refactory

--- ApiFacebook.ConsoleAut/Program.cs
+++ ApiFacebook.ConsoleAut/Program.cs
@@ -17,21 +17,21 @@ namespace ApiFacebook.ConsoleAut
         {
 
             Program program = new Program();
-            
+
             var querosabe = program.Get2();
 
-            var OK = querosabe;
+            var ok = querosabe;
 
         }
 
 
-        public Task<string> Get2()
+        public HotmartToken Get2()
         {
             return GetAuthorizeTokenHotmart();
         }
 
 
-        public static async Task<string> GetAuthorizeTokenHotmart()
+        public static HotmartToken GetAuthorizeTokenHotmart()
         {
             // Initialization.  
             string responseObj = string.Empty;
@@ -39,30 +39,6 @@ namespace ApiFacebook.ConsoleAut
             string clientId = "34c73160-127f-417e-b133-275bc0d2c3ad";
             string clientSecret = "a74779a0-cd27-4896-86af-2940ccabeaf6";
             string Basic = "Basic MzRjNzMxNjAtMTI3Zi00MTdlLWIxMzMtMjc1YmMwZDJjM2FkOmE3NDc3OWEwLWNkMjctNDg5Ni04NmFmLTI5NDBjY2FiZWFmNg==";
-
-            // Posting.  
-            using (var client = new HttpClient())
-            {
-                // Setting Base address.  
-                client.BaseAddress = new Uri("https://api-sec-vlc.hotmart.com/security/oauth/token?grant_type=client_credentials&client_id=34c73160-127f-417e-b133-275bc0d2c3ad&client_secret=a74779a0-cd27-4896-86af-2940ccabeaf6");
-
-                // Setting content type.  
-                client.DefaultRequestHeaders.Add("Authorization", Basic);
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                
-                //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
-
-              
-                var request = new HttpRequestMessage()
-                {
-                    Method = HttpMethod.Post,
-                    RequestUri = new Uri("https://api-sec-vlc.hotmart.com/security/oauth/token?grant_type=client_credentials&client_id=34c73160-127f-417e-b133-275bc0d2c3ad&client_secret=a74779a0-cd27-4896-86af-2940ccabeaf6")
-
-                };
-
-                //request.Headers.Add("Content-Type", "application/json");
-                request.Headers.Add("Authorization", Basic);
-
 
                 string URL = "https://api-sec-vlc.hotmart.com/security/oauth/token?grant_type=client_credentials&client_id=34c73160-127f-417e-b133-275bc0d2c3ad&client_secret=a74779a0-cd27-4896-86af-2940ccabeaf6";
                 var request2 = new RestRequest(URL, Method.POST, DataFormat.Json);
@@ -75,74 +51,8 @@ namespace ApiFacebook.ConsoleAut
 
                 var tokenHotmart = JsonConvert.DeserializeObject<HotmartToken>(resposta.Content);
 
+            return tokenHotmart;
                 
-
-                //var response = await client.po
-                // HttpResponseMessage response = await client.SendAsync(request);
-                // HttpContent content = response.Content;
-
-
-                //try
-                //{
-
-                //   // var response = await client.SendAsync(request);
-
-                //    if (responta.IsSuccessStatusCode)
-                //    {
-                //        // Reading Response.  
-
-                //    }
-                //    else
-                //    {
-
-                //    }
-                //}
-                //catch (Exception)
-                //{
-
-                //    throw;
-                //}
-
-
-                //var byteArray = Encoding.ASCII.GetBytes("YTBiZmU2NTktNzJkMC00OGFkLWE5ZTQtOTRmZTdkZDBiOWM4OjZjMTE5YzFkLTFjNjctNDQ2MS1hMTBmLTFhMTNjN2FkMmQ0ZQ==");
-                //client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic: ", Convert.ToBase64String(byteArray));
-
-                //var request = new HttpRequestMessage()
-                //{
-                //    Method = HttpMethod.Post,
-                //    RequestUri = new Uri("https://api-sec-vlc.hotmart.com/security/oauth/token?grant_type=client_credentials&client_id=:a0bfe659-72d0-48ad-a9e4-94fe7dd0b9c8:&client_secret=:6c119c1d-1c67-4461-a10f-1a13c7ad2d4e")
-
-                //};
-
-                //request.Headers.Add("Content-Type", "application/json");
-                //request.Headers.Add("Authorization", "Basic :YTBiZmU2NTktNzJkMC00OGFkLWE5ZTQtOTRmZTdkZDBiOWM4OjZjMTE5YzFkLTFjNjctNDQ2MS1hMTBmLTFhMTNjN2FkMmQ0ZQ==");
-
-
-                //var response = await client.GetStringAsync(client.BaseAddress);
-
-                //HttpResponseMessage response = await client.SendAsync(request);
-
-                //  HttpContent content = response.Content;
-
-
-
-                // Initialization.  
-                //HttpResponseMessage response = new HttpResponseMessage();
-                // List<KeyValuePair<string, string>> allIputParams = new List<KeyValuePair<string, string>>();
-
-                // Convert Request Params to Key Value Pair.  
-
-                // URL Request parameters.  
-                //  HttpContent requestParams = new FormUrlEncodedContent(allIputParams);
-
-                // HTTP POST  
-                //response = await client.PostAsync(client.BaseAddress, content).ConfigureAwait(false);
-
-                // Verification  
-
-            }
-
-            return responseObj;
         }
 
 
---
+++ ApiFacebook.InfraData/Repository/HotmartRepository.cs
@@ -0,0 +1,29 @@
+﻿using ApiFacebook.ConsoleAut.EntityAutent;
+using ApiFacebook.InfraData.Data;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFacebook.InfraData.Repository
+{
+    public class HotmartRepository: Conexao
+    {
+
+        public void LimparToken()
+        {
+            using (IDbConnection cn = Connection)
+            {
+
+                cn.Open();
+
+                var listFriends = cn.Query<HotmartToken>("Select * From tblFriends").ToList();
+                
+            }
+        }
+
+    }
+}
