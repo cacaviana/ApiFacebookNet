68964bd9e9817b91f0ad28f0aa9e339f9cb98807
Carlos Viana <carlosaraujoviana@outlook.com>
2021-12-29 22:32:14 -0500

    Acrescentei o pacote RestSharp

---
+++ ApiFacebook.ConsoleAut/EntityAutent/HotmartToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFacebook.ConsoleAut.EntityAutent
+{
+    public class HotmartToken
+
+
+    
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public string expires_in { get; set; }
+        public string scope { get; set; }
+        public string jti { get; set; }
+
+    }
+}
--- ApiFacebook.ConsoleAut/Program.cs
+++ ApiFacebook.ConsoleAut/Program.cs
@@ -5,6 +5,9 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using ApiFacebook.ConsoleAut.EntityAutent;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace ApiFacebook.ConsoleAut
 {
@@ -45,7 +48,9 @@ namespace ApiFacebook.ConsoleAut
 
                 // Setting content type.  
                 client.DefaultRequestHeaders.Add("Authorization", Basic);
-                client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
 
               
                 var request = new HttpRequestMessage()
@@ -55,35 +60,49 @@ namespace ApiFacebook.ConsoleAut
 
                 };
 
-                request.Headers.Add("Content-Type", "application/json");
+                //request.Headers.Add("Content-Type", "application/json");
                 request.Headers.Add("Authorization", Basic);
 
 
-               // var response = await client.SendAsync(request);
-               // HttpResponseMessage response = await client.SendAsync(request);
+                string URL = "https://api-sec-vlc.hotmart.com/security/oauth/token?grant_type=client_credentials&client_id=34c73160-127f-417e-b133-275bc0d2c3ad&client_secret=a74779a0-cd27-4896-86af-2940ccabeaf6";
+                var request2 = new RestRequest(URL, Method.POST, DataFormat.Json);
+
+                request2.AddHeader("Authorization", Basic);
+                request2.AddHeader("Content-Type", "application /json");
+
+                var clientRest = new RestClient();
+                var resposta = clientRest.Post<HotmartToken>(request2);
+
+                var tokenHotmart = JsonConvert.DeserializeObject<HotmartToken>(resposta.Content);
+
+                
+
+                //var response = await client.po
+                // HttpResponseMessage response = await client.SendAsync(request);
                 // HttpContent content = response.Content;
 
 
-                try
-                {
-                    var response = await client.SendAsync(request);
+                //try
+                //{
 
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // Reading Response.  
+                //   // var response = await client.SendAsync(request);
 
-                    }
-                    else
-                    {
+                //    if (responta.IsSuccessStatusCode)
+                //    {
+                //        // Reading Response.  
 
-                    }
-                }
-                catch (Exception)
-                {
+                //    }
+                //    else
+                //    {
 
-                    throw;
-                }
-                
+                //    }
+                //}
+                //catch (Exception)
+                //{
+
+                //    throw;
+                //}
+
 
                 //var byteArray = Encoding.ASCII.GetBytes("YTBiZmU2NTktNzJkMC00OGFkLWE5ZTQtOTRmZTdkZDBiOWM4OjZjMTE5YzFkLTFjNjctNDQ2MS1hMTBmLTFhMTNjN2FkMmQ0ZQ==");
                 //client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic: ", Convert.ToBase64String(byteArray));
@@ -120,7 +139,7 @@ namespace ApiFacebook.ConsoleAut
                 //response = await client.PostAsync(client.BaseAddress, content).ConfigureAwait(false);
 
                 // Verification  
-               
+
             }
 
             return responseObj;
